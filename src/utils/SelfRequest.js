252cc8009ec29adcdeb9328391cd34af203176c7
SelfRidicule <selfridicule@gmail.com>
2019-10-03 12:58:35 +0800

    Axios 拦截器配置

--- src/utils/SelfRequest.js
+++ src/utils/SelfRequest.js
@@ -28,6 +28,32 @@ const instance = axios.create(config);
 
 
 /**
+ * 拦截器
+ */
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    console.log('interceptors.request==================>' + JSON.stringify(config))
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    console.error('interceptors.request.error==================>' + JSON.stringify(error))
+    return Promise.reject(error);
+  });
+
+// 添加响应拦截器
+instance.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    console.log('interceptors.response==================>' + JSON.stringify(response))
+    return response;
+  }, function (error) {
+    // 对响应错误做点什么
+    console.error('interceptors.response.error==================>' + JSON.stringify(error))
+    return Promise.reject(error);
+  });
+
+
+/**
  * 请求配置
  */
 let requestConfig = {
