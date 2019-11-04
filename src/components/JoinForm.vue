b49f0af614cdcbe526085823f1eb267a7b2a9714
SelfRidicule <SelfRidicule@gmail.com>
2019-11-04 17:53:44 +0800

    VueSignaturePad 签名组件 : 实现下载图片

--- src/components/JoinForm.vue
+++ src/components/JoinForm.vue
@@ -26,6 +26,8 @@
       
     </van-cell-group>
 
+    <img :src="imgUrl"  style="width:100px ; height:100px"/>
+
     <!-- 签名组件 -->
     <VueSignaturePad
       width="80%"
@@ -42,6 +44,11 @@
     确定
     </van-button>
     
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+
   </div>
 </template>
 
@@ -61,6 +68,7 @@ export default {
         border: 'gray 1px solid',
          margin : '20px auto'
       },
+      imgUrl : '',
     }
   },
   /**
@@ -95,9 +103,57 @@ export default {
 
       const { isEmpty, data } = this.$refs.signaturePad.saveSignature();
       console.log(isEmpty);
-      console.log(data);
+      console.log(this.convertBase64UrlToBlob(data));
+      console.log(window.URL.createObjectURL( this.convertBase64UrlToBlob(data) ));
 
+      this.imgUrl =  window.URL.createObjectURL( this.convertBase64UrlToBlob(data) ) ;
+     
+      this.downloadFile(this.convertBase64UrlToBlob(data) , 'pic')
     },
+
+    /**
+     * 将以base64的图片url数据转换为Blob
+     */
+    convertBase64UrlToBlob(urlData){
+
+       //去掉url的头，并转换为byte
+      let bytes = window.atob(urlData.split(',')[1]);       
+        
+      //处理异常,将ascii码小于0的转换为大于0
+      let ab = new ArrayBuffer(bytes.length);
+      let ia = new Uint8Array(ab);
+      for (let i = 0; i < bytes.length; i++) {
+          ia[i] = bytes.charCodeAt(i);
+      }
+
+      return new Blob( [ab] , {type : 'image/png'});
+    },
+   
+    /**
+     * 下载文件
+     * 参数 :
+     *  Blob 对象
+     *  fileName 文件名
+     */
+    downloadFile(blob , fileName){
+
+      //Blob 对象的URL
+      let url = window.URL.createObjectURL(blob)
+
+      //创建a标签
+      let link = document.createElement('a');
+      //设置样式
+      link.style.display = 'none';
+      //设置url
+      link.href = url;
+      //设置属性
+      link.setAttribute('download', fileName + '.png');
+      //添加到body标签
+      document.body.appendChild(link);
+      //a标签点击
+      link.click();
+    },
+
   },
 }
 </script>
